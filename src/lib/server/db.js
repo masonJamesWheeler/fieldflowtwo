5edb14d3dc1f35b50ef97799ecbcff828edaed4f
wheels <mwheel@uw.edu>
2024-06-30 14:42:04 -0700

    Fixed security issues

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,11 +1,20 @@
 // src/lib/server/db.js
 import pkg from 'pg';
+// get env variables
+import dotenv from 'dotenv';
+
+// get username and password
+dotenv.config();
+
+let user = process.env.DB_USER;
+let password = process.env.DB_PASS;
+console.log(user, password);
 
 const {Pool} = pkg;
 
 const pool = new Pool({
-    user: "masewheeler",
-    password: "Brewen12!!",
+    user: user,
+    password: password,
     host: "localhost",
     port: 5432,
     database: "fieldflow",
